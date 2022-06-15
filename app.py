03c169177690f3a47e6f18f53c86793f6c7fd85a
Anand <patilanand867@gmail.com>
2022-06-15 23:51:49 +0530

    first

---
+++ app.py
@@ -0,0 +1,68 @@
+from fastapi import Form, File, UploadFile, Request, FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, session
+from apps import database, pre_designed_urls, schemas, model, crud
+# from typing import List
+# from fastapi.responses import HTMLResponse
+# from pydantic import BaseModel
+# from typing import Optional, List
+#from fastapi.templating import Jinja2Templates
+# import requests
+# import tempfile
+# import os
+basepath='/Users/hash/'
+
+engine = database.engine
+
+model.Base.metadata.create_all(bind=engine)
+
+#app1 = Flask(__name__)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/upload")
+async def upload(base: schemas.Base = Depends(), file: UploadFile = File(...), db: session = Depends(get_db)):
+    data = (await file.read()).decode("utf-8")
+    path = basepath+file.filename
+    f = open(path, "a")
+    f.write(data)
+    f.close()
+    URL = pre_designed_urls.generate(file.filename,basepath)
+    print("********",URL)
+    received_data = base.dict()
+    print(received_data)
+    return crud.upload(db=db, user=base, file=URL)
+
+
+#@app.get("/", response_class=HTMLResponse)
+#def main(request: Request):
+ #   return templates.TemplateResponse("index.html", {"request": request})
+
+# @app.post("/login")
+# async def login(user: schemas.UserCreate):
+#     email=user.email
+#     password=user.password
+#     return {"email":email, "passord":password}
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
